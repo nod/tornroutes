4a1419c9c41caf61e36be72932118fee1aeb31bf
Jeremy Kelley <jeremy@collectivelabs.com>
2011-11-18 22:54:12 -0600

    correcting error with setup.py.  no functional change

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='tornado_addons',
-      version='0.02',
-      description='Tornado Web Add-ons',
+      version='0.02.1',
+      description='Tornado Web Route Decorator',
       author='Jeremy Kelley',
-      author_email='jeremy@collectivelabs.com',
+      author_email='jeremy@33ad.org',
       url='https://github.com/nod/tornado_addons',
-      packages=['tornado_addons'],
-      package_dir={'tornado_addons':'tornado_addons'},
+      packages=['tornroutes'],
+      package_dir={'tornroutes':'tornroutes'},
      )
 
