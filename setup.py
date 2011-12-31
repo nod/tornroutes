94d8ac8ed5a47d6b90c7b0ae72dfd47e1a077882
Jeremy Kelley <jeremy@collectivelabs.com>
2011-12-31 14:51:47 -0600

    correcting setup.py once again

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from distutils.core import setup
 
-setup(name='tornroutes',
-      version='0.02.1',
-      description='Tornado Web Route Decorator',
-      author='Jeremy Kelley',
-      author_email='jeremy@33ad.org',
-      url='https://github.com/nod/tornroutes',
-      packages=['tornroutes'],
-      package_dir={'tornroutes':'tornroutes'},
-     )
+setup(
+    name='tornroutes',
+    version='0.02.2',
+    description='Tornado Web Route Decorator',
+    author='Jeremy Kelley',
+    author_email='jeremy@33ad.org',
+    url='https://github.com/nod/tornroutes',
+    py_modules=['tornroutes']
+    )
 
