aaf6668aecda8425dd5fc490af054035c9289ce9
Jeremy Kelley <jeremy@collectivelabs.com>
2011-12-31 14:53:23 -0600

    minor bump in version for pkg changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='tornroutes',
-    version='0.02.2',
+    version='0.02.3',
     description='Tornado Web Route Decorator',
     author='Jeremy Kelley',
     author_email='jeremy@33ad.org',
