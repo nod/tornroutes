c314bb8fa52b62597272ae399e97c05bf7d5479c
Jeremy Kelley <jeremy@collectivelabs.com>
2012-01-01 22:25:28 -0600

    still fighting with setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from distutils.core import setup
 VERSION = open('VERSION').read().lstrip('version: ').rstrip('\n')
 
 setup(
-    version=VERSION,
-    name='tornroutes',
-    description='Tornado Web Route Decorator',
-    author='Jeremy Kelley',
-    author_email='jeremy@33ad.org',
-    url='https://github.com/nod/tornroutes',
-    packages=['tornroutes'],
-    install_requires=['tornado',]
+    name = 'tornroutes',
+    version = VERSION,
+    description = 'Tornado Web Route Decorator',
+    author = 'Jeremy Kelley',
+    author_email = 'jeremy@33ad.org',
+    url = 'https://github.com/nod/tornroutes',
+    license = "http://www.apache.org/licenses/LICENSE-2.0",
+    packages = ['tornroutes'],
+    package_dir={'mypkg': 'src/mypkg'},
+    install_requires = ['tornado',]
     )
 
