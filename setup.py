bb53cfea88e52c695666094decb28d5a04f9992f
Jeremy Kelley <jeremy@collectivelabs.com>
2012-01-01 21:32:04 -0600

    further package refactor

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@
 
 from distutils.core import setup
 
+VERSION = open('VERSION').read().lstrip('version: ').rstrip('\n')
+
 setup(
+    version=VERSION,
     name='tornroutes',
-    version='0.02.3',
     description='Tornado Web Route Decorator',
     author='Jeremy Kelley',
     author_email='jeremy@33ad.org',
---
+++ src/tornroutes/__init__.py
@@ -0,0 +1,1 @@
+from .tornroutes import route, route_redirect
